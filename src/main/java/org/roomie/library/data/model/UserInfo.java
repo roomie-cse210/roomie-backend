979f493c2650b954447ba03a8557b6d589cfc3ea
SIMRAN GILL <gillsimu98@gmail.com>
2023-02-15 15:04:55 -0800

    connecting to remote dynamodb

--- src/main/java/org/roomie/library/data/model/UserInfo.java
+++ src/main/java/org/roomie/library/data/model/UserInfo.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 
-@DynamoDBTable(tableName = "Users")
+@DynamoDBTable(tableName = "UserTable")
 public class UserInfo {
 	private String username;
 	private String password;
