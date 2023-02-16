ed97cf515a7050dff55ea43a42e5dc5756a2c0f3
harshgondaliya <harshgondaliya99@gmail.com>
2023-02-15 22:55:24 -0800

    added /forgotPassword API; left to add code to send email

--- src/main/java/org/roomie/library/web/RoomieController.java
+++ src/main/java/org/roomie/library/web/RoomieController.java
@@ -1,11 +1,12 @@
 package org.roomie.library.web;
-
 import org.roomie.library.data.model.UserInfo;
+import java.util.Random;
 import org.roomie.library.data.repositories.UserInfoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class RoomieController {
@@ -43,7 +45,7 @@ public class RoomieController {
 
 	@PostMapping("/createUser")
 	public ResponseEntity<String> createUser(@RequestBody UserInfo userInfo) {
-		logger.info("Checking whether user is already registered");
+		logger.info("Checking whether email is already registered");
 		var val = userInfoRepository.findById(userInfo.getEmail());
 		if (val.isPresent()) {
 			logger.info("User {} is already registered", userInfo.getEmail());
@@ -57,14 +59,34 @@ public class RoomieController {
 
 	@PostMapping("/updateUser")
 	public ResponseEntity<String> updateUser(@RequestBody UserInfo userInfo) {
-		logger.info("Checking whether user is registered");
+		logger.info("Checking whether email is registered");
 		var val = userInfoRepository.findById(userInfo.getEmail());
 		if (!val.isPresent()) {
 			logger.info("User {} is not registered", userInfo.getEmail());
 			return ResponseEntity.status(419).body("user not registered");
 		} else {
 			var uinfo = userInfoRepository.save(userInfo);
-			logger.info("Updated user {} successfully", uinfo.getEmail());
+			logger.info("Updated user {} password to {}", uinfo.getEmail(), uinfo.getPassword());
+			return ResponseEntity.status(200).body("user updated");
+		}
+	}
+	@PostMapping("/forgotPassword")
+	public ResponseEntity<String> forgotPassword(@RequestBody UserInfo userInfo) {
+		logger.info("Checking whether email is registered");
+		var val = userInfoRepository.findById(userInfo.getEmail());
+		if (!val.isPresent()) {
+			logger.info("User {} is not registered", userInfo.getEmail());
+			return ResponseEntity.status(419).body("user not registered");
+		} else {
+			// generate random password
+			String randomPassword = generateRandomString();
+			logger.info("Generated random password {}", randomPassword);
+			// send email to user sharing random password
+
+			// update the DB to use random password for user
+			userInfo.setPassword(randomPassword);
+			var uinfo = userInfoRepository.save(userInfo);
+			logger.info("Updated user {} password to {}", uinfo.getEmail(), randomPassword);
 			return ResponseEntity.status(200).body("user updated");
 		}
 	}
@@ -74,5 +96,35 @@ public class RoomieController {
 		StringBuilder sb = new StringBuilder();
     	userInfoRepository.findAll().forEach(sb::append);
 		return ResponseEntity.ok(sb.toString());
+	}
+
+	static String generateRandomString(){
+		// create a string of all characters
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		// create random string builder
+		StringBuilder sb = new StringBuilder();
+	
+		// create an object of Random class
+		Random random = new Random();
+	
+		// specify length of random string
+		int length = 7;
+	
+		for(int i = 0; i < length; i++) {
+	
+		  // generate random index number
+		  int index = random.nextInt(alphabet.length());
+	
+		  // get character specified by index
+		  // from the string
+		  char randomChar = alphabet.charAt(index);
+	
+		  // append the character to string builder
+		  sb.append(randomChar);
+		}
+	
+		String randomString = sb.toString();
+		return randomString;
 	}
 }
