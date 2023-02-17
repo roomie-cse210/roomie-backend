8023b771111b23a546d84c8751383d15c91c3e73
harshgondaliya <harshgondaliya99@gmail.com>
2023-02-16 20:59:07 -0800

    added /emailOTP API

--- src/main/java/org/roomie/library/data/services/EmailSenderService.java
+++ src/main/java/org/roomie/library/data/services/EmailSenderService.java
@@ -15,19 +15,32 @@ public class EmailSenderService {
 
     private static final Logger logger = LoggerFactory.getLogger( EmailSenderService.class);
 
-    public String getBody(String newpassword){
-        return "Hey!! \n\nYour Roomie password has been reset to \"" + newpassword + "\". Kindly use it to login. \n\nRegards \n\nTeam Roomie";
+    public String getResetPasswordBody(String newpassword){
+        return "Hey!! \n\nYour Roomie password has been reset to \"" + newpassword + "\". Kindly use it to login. \n\nRegards, \nTeam Roomie";
+    }
+    public String getEmailOTPBody(String OTP){
+        return "Hey!! \n\n OTP to validate your roomie registration is \"" + OTP + "\". Kindly use it to validate your registration. \n\nRegards, \nTeam Roomie";
     }
 
-    public void sendSimpleEmail(String toEmail,
+    public void sendResetPasswordEmail(String toEmail,
                                 String newpassword) {
         
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
-        message.setText(getBody(newpassword));
+        message.setText(getResetPasswordBody(newpassword));
         message.setSubject("Password reset for Roomie");
         mailSender.send(message);
-        logger.info("Reset password mail sent to: " + toEmail);
+        logger.info("Reset password email sent to: " + toEmail);
+    }
+
+    public void sendOTPEmail(String toEmail,
+                                String OTP) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setText(getEmailOTPBody(OTP));
+        message.setSubject("OTP for Roomie Registration");
+        mailSender.send(message);
+        logger.info("OTP email sent to: " + toEmail);
     }
 
     
